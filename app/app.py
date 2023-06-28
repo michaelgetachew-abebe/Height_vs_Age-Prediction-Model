6bb477a6e4395a4e253ec4d08486bd7e9945e2a7
michaelgetachew-abebe <mikygetyos@gmail.com>
2023-06-29 02:05:24 +0300

    Feat(Add): added a simple linear regression model and exposed a test model inference through a flask app

--- app/app.py
+++ app/app.py
@@ -1,6 +1,16 @@
 from flask import Flask
+import numpy as np
+from joblib import load
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello World!"
+    test_np_input = np.array([[1], [2], [17]])
+    with open('./model.joblib', 'rb') as read_in:
+        model = load(read_in)
+
+    predictions = model.predict(test_np_input)
+    pred_as_str = str(predictions)
+
+    return pred_as_str
